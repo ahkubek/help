9865aa11c1c2d0b9e852860915ad31d82bafffd3
ahkubek <120653318+ahkubek@users.noreply.github.com>
2024-01-17 20:10:14 +0300

    help1

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,23 @@
 import  java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws java.io.IOException {
-        char ch;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("справка:");
+        char choise, ignore;
+        do {
+            // Scanner sc = new Scanner(System.in);
+            System.out.println("справка:");
 
             System.out.println("1.if");
             System.out.println("2.switch:");
+            System.out.println("3.for");
+            System.out.println("4.while");
+            System.out.println("5.do-while\n");
             System.out.println("выберите");
-            ch = (char) System.in.read();
-
-            switch (ch) {
+            choise = (char) System.in.read();
+            do {
+                ignore = (char) System.in.read();
+            } while (ignore != '\n');
+        }while (choise<'1'|choise>'5');
+            switch (choise) {
                 case '1':
                     System.out.println(" if\n");
                     System.out.println("if (условие) инструкция;");
@@ -25,8 +32,10 @@ public class Main {
                     System.out.println("//...");
                     System.out.println("}");
                     break;
-                default:
-                    System.out.println("Запрос не найден");
+                case '3':
+                    System.out.println("цикл for: \n");
+                    System.out.println("инициализация;условие; итерация");
+                    System.out.println("инструкция");
             }
         }
     }
